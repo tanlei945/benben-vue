0a105dbc6d5a1fbf5e5ae94b66323ef22f333665
wanghao <wanghao@8b70f4e1-b3c0-4b97-97da-73496876a715>
2019-04-22 02:03:57 +0000

    修改相关页面

--- frontend/benben-vue/src/components/layouts/UserLayout.vue
+++ frontend/benben-vue/src/components/layouts/UserLayout.vue
@@ -9,7 +9,7 @@
           </a>
         </div>
         <div class="desc">
-          Jeecg Boot 是中国最具影响力的 企业级 快速开发平台
+          BenBen Boot 企业级 快速开发平台
         </div>
       </div>
 
@@ -22,7 +22,7 @@
           <a href="https://github.com/zhangdaiscott/jeecg-boot" target="_blank">条款</a>
         </div>
         <div class="copyright">
-          Copyright &copy; 2019 <a href="http://www.jeecg.org" target="_blank">JEECG开源社区</a> 出品
+          Copyright &copy; 2019
         </div>
       </div>
     </div>
